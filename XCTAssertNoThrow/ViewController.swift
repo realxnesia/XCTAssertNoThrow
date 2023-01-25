89b61ab4c764504c35492e8f6da0a959c46fe5f1
dhika <dhika.aditya@kpvendor.id>
2023-01-25 16:23:25 +0700

    Initial Commit

---
+++ XCTAssertNoThrow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCTAssertNoThrow
+//
+//  Created by realxnesia on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
